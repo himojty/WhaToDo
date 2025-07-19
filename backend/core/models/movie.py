5330ae4b1fcbc73fbaabfe34409a5c0d0255baaa
himojty <mtryrd.13@gmail.com>
2025-07-20 00:55:47 +0300

    Create movie model

---
+++ backend/core/models/movie.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text
+
+from base import Base
+from mixins import IdIntPkMixin
+
+
+class Movie(Base, IdIntPkMixin):
+
+    title: Mapped[str] = mapped_column(
+        String(128),
+    )
+    origin_title: Mapped[str | None] = mapped_column(
+        String(128),
+    )
+    description: Mapped[str | None] = mapped_column(
+        Text(),
+    )
+    release: Mapped[str | None]
