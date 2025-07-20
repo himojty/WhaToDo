9e41614680718f8409888207d0455c177e6bf4e9
himojty <mtryrd.13@gmail.com>
2025-07-20 15:36:50 +0300

    init vite create-react-app & clear trash

--- frontend/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('.header');
-  if (window.scrollY > 50) { // 50 - это порог скролла, можно изменить
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove('sticky');
-  }
-});
