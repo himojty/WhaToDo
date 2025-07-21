f81f5cc479e317c6f601e98c5ad61a7898c92a62
himojty <mtryrd.13@gmail.com>
2025-07-21 11:41:16 +0300

    fix import with @

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@shared': path.resolve(__dirname, './src/shared'),
+            '@features': path.resolve(__dirname, './src/features'),
+        },
+    },
+});
