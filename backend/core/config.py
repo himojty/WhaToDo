ae6a5aa040dee27501f99d8e18adcbabea5b3370
ACER <himojty@gmail.com>
2025-07-22 21:52:21 +0300

    create db settings

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,8 +1,17 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
+import os
 
+
+class DBSettings(BaseModel):
+    url: str = os.environ.get("DATABASE_URL")
+    echo: bool = False
 
 
 class Settings(BaseSettings):
-    api_prefix: str = '/api/'
+    api_prefix: str = "/api/"
+
+    db: DBSettings = DBSettings()
+
 
 settings = Settings()
