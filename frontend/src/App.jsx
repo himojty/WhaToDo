179e3cf519f06e23da431144fe144aeb3c6d3f1e
himojty <mtryrd.13@gmail.com>
2025-07-22 14:56:11 +0300

    add new \movies rout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import HomePage from "./pages/Home.jsx";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import MoviesPage from "@/pages/Movies.jsx";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
+                <Route path="/" element={<MoviesPage/>}/>
             </Routes>
         </Router>
     );
