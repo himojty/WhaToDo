d57ea575f4cedc65751147086c525510e3d8ceef
himojty <mtryrd.13@gmail.com>
2025-07-22 11:12:24 +0300

    create footer

---
+++ frontend/src/shared/ui/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footerContainer}>
+                <p>© 2025</p>
+            </div>
+        </footer>
+    )
+}
