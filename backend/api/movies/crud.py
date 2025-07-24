075df5048b2764f3ca2dc2be3ef86ef0350d6693
himojty <mtryrd.13@gmail.com>
2025-07-24 16:52:03 +0300

    Add get movies

--- backend/api/movies/crud.py
+++ backend/api/movies/crud.py
@@ -1,10 +1,17 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, Result
 
 from core.models import Movie
 
 
-async def create_movie(session: AsyncSession, movie_in: dict):
+async def create_movie(session: AsyncSession, movie_in: dict) -> Movie:
     movie = Movie(**movie_in.model_dump())
     session.add(movie)
     await session.commit()
     return movie
+
+async def get_movies(session: AsyncSession) -> list[Movie] | None:
+    stmt = select(Movie).order_by(Movie.id)
+    result: Result = await session.execute(stmt)
+    movies = result.scalars().all()
+    return movies
