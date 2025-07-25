af1a6c9af2bf3aa0f46f4d0b07ab551347c9d1e6
himojty <mtryrd.13@gmail.com>
2025-07-25 15:55:35 +0300

    New: add api settings

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
+class ApiSettings(BaseModel):
+    prefix: str = "/api"
 
 class DbSettings(BaseModel):
     url: str = "postgresql+asyncpg://app:password@localhost:5432/film-library"
@@ -8,8 +10,7 @@ class DbSettings(BaseModel):
 
 
 class Settings(BaseSettings):
-    api_prefix: str = "/api/"
-
+    api: ApiSettings = ApiSettings()
     db: DbSettings = DbSettings()
 
 
