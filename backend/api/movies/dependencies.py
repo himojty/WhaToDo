e449980745cf7c9b9b56c1c54c3c051f203944e6
himojty <mtryrd.13@gmail.com>
2025-07-25 16:57:02 +0300

    New: add movie id dependency

---
+++ backend/api/movies/dependencies.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import Path, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper, Movie
+from . import crud
+
+
+async def movie_by_id(
+    movie_id: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> Movie:
+    movie = await crud.get_movie_by_id(session=session, movie_id=movie_id)
+    if movie is not None:
+        return movie
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Product {article_id} not found!",
+    )
