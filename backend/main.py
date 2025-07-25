a5ebea66d5f5305e9981d31bef2b4f8849d1a209
himojty <mtryrd.13@gmail.com>
2025-07-25 15:15:22 +0300

    New: add prefix to api router

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 import uvicorn
 
 from api import router as api_router
-
+from backend.core.config import settings
 
 app = FastAPI()
-app.include_router(router=api_router)
+app.include_router(router=api_router, prefix=settings.api_prefix)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app")
