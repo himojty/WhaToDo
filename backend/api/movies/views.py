c691175ee01dbb41c443bd9719f7b8113bf87b88
himojty <mtryrd.13@gmail.com>
2025-07-25 13:11:41 +0300

    New: add post view of create movie

---
+++ backend/api/movies/views.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.db_helper import db_helper
+from core.schemas.movies import MovieCreate
+from . import crud
+
+router = APIRouter(prefix="/movies", tags=["Movies"])
+
+
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_movie(
+    movie: MovieCreate,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return crud.create_movie(session=session, movie_in=movie)
