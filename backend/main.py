662b85bf35efb38a9ce05990708e05b67fcfb288
himojty <mtryrd.13@gmail.com>
2025-07-25 16:10:56 +0300

    Patch: change prefix usage

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from api import router as api_router
 from core.config import settings
 
 app = FastAPI()
-app.include_router(router=api_router, prefix=settings.api_prefix)
+app.include_router(router=api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app")
