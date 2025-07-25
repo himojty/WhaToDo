969e360bf0ff9d1d96bd41e141fb4ef9f6351da2
himojty <mtryrd.13@gmail.com>
2025-07-25 16:10:21 +0300

    Fix: import config

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 
 from api import router as api_router
-from backend.core.config import settings
+from core.config import settings
 
 app = FastAPI()
 app.include_router(router=api_router, prefix=settings.api_prefix)
