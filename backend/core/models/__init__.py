2028534d20dd26badecb1caf7f4fe511e0751779
himojty <mtryrd.13@gmail.com>
2025-07-28 01:52:31 +0300

    New: create rating model

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     "Base",
     "Movie",
     "Site",
+    "Rating",
     "db_helper",
 )
 
 from .base import Base
 from .movie import Movie
 from .site import Site
+from .rating import Rating
 from .db_helper import db_helper
