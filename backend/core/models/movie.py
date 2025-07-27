3f1fd1c9534722b5a0eb7a1b70e3a777dd883341
himojty <mtryrd.13@gmail.com>
2025-07-28 00:03:16 +0300

    New: chain relationship between movie and sites

--- backend/core/models/movie.py
+++ backend/core/models/movie.py
@@ -1,8 +1,13 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text, LargeBinary
 
 from .base import Base
 from .mixins import IdIntPkMixin
+
+if TYPE_CHECKING:
+    from .sites import Site
 
 
 class Movie(Base, IdIntPkMixin):
@@ -15,3 +20,5 @@ class Movie(Base, IdIntPkMixin):
     )
     release: Mapped[str | None]
     image: Mapped[bytes | None] = mapped_column(LargeBinary())
+
+    sites: Mapped["Site"] = relationship(back_populates="movies")
