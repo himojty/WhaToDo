3a8e09743fef04dee5f56c6a9f669df7787b63b8
himojty <mtryrd.13@gmail.com>
2025-07-28 23:54:42 +0300

    New: create get and post views for ratings

---
+++ backend/api/ratings/views.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.schemas.ratings import RatingCreate
+from . import crud
+from core.models import db_helper
+
+router = APIRouter(prefix="/ratings", tags=["Ratings"])
+
+
+@router.get("/")
+async def get_ratings(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    await crud.get_ratings(session=session)
+
+
+@router.post("/")
+async def create_rating(
+    rating_in: RatingCreate,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.create_rating(session=session, rating_in=rating_in)
