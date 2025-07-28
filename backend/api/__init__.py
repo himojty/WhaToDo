cba0cb434fdb58063372287255a5f2b697520b88
himojty <mtryrd.13@gmail.com>
2025-07-29 00:01:40 +0300

    New: connect ratings view to main app

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .movies.views import router as movies_router
+from .ratings.views import router as ratings_router
 
 router = APIRouter()
 router.include_router(router=movies_router)
+router.include_router(router=ratings_router)
