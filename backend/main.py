4abfcbb7ebf48775fbc07e88e85da830dc41c566
himojty <mtryrd.13@gmail.com>
2025-07-31 01:17:24 +0300

    TMP: add cors

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 
 from api import router as api_router
 from core.config import settings
 
 app = FastAPI()
 app.include_router(router=api_router, prefix=settings.api.prefix)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ['*'] Разрешить все домены
+    allow_methods=["GET"],  # ['*'] Разрешить все методы (GET, POST и т.д.)
+    # allow_headers=["*"],  # Разрешить все заголовки
+)
 
 if __name__ == "__main__":
     uvicorn.run(
