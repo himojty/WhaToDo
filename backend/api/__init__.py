119d9197764fcc633ce39d3d82832d220304f217
himojty <mtryrd.13@gmail.com>
2025-07-31 01:21:06 +0300

    Chore: add sites router to main app

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from .movies.views import router as movies_router
 from .ratings.views import router as ratings_router
+from .sites.views import router as sites_router
 
 router = APIRouter()
 router.include_router(router=movies_router)
 router.include_router(router=ratings_router)
+router.include_router(router=sites_router)
