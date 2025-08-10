a401a10e8b032910898c4dfb5c02c5a3de029914
himojty <mtryrd.13@gmail.com>
2025-08-10 17:44:30 +0300

    New: add images router to main router

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from .movies.views import router as movies_router
 from .ratings.views import router as ratings_router
 from .sites.views import router as sites_router
+from .images.views import router as images_router
 
 router = APIRouter()
 router.include_router(router=movies_router)
 router.include_router(router=ratings_router)
 router.include_router(router=sites_router)
+router.include_router(router=images_router)
