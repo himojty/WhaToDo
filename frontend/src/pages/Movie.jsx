98d2eec011f7facfc17c959843429750ebc33db9
himojty <mtryrd.13@gmail.com>
2025-08-10 17:45:44 +0300

    New: create movie page

---
+++ frontend/src/pages/Movie.jsx
@@ -0,0 +1,10 @@
+import Layout from '@layouts/MainLayout/index.jsx';
+import MovieSingle from "@widgets/MovieSingle/MovieSingle.jsx";
+
+export default function MoviePage() {
+    return (
+        <Layout>
+            <MovieSingle/>
+        </Layout>
+    );
+}
