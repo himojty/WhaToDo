3325abdea18fa2319dfd3aa37ba02f488172d00d
himojty <mtryrd.13@gmail.com>
2025-08-10 17:46:25 +0300

    New: create movies page with MoviesList.jsx

--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -1,8 +1,10 @@
 import Layout from '@layouts/MainLayout/index.jsx';
+import MoviesList from "@widgets/MoviesList/MoviesList.jsx";
 
 export default function MoviesPage() {
     return (
         <Layout>
+            <MoviesList/>
         </Layout>
     );
 }
